9fe7f0fb474f8e318102963dd3dbd77b55f716f6
Smilzo97 <Smilzo97@users.noreply.github.com>
2022-01-16 19:15:52 +0100

    Import e scelta

--- impiccato.py
+++ impiccato.py
@@ -1,8 +1,29 @@
 import pandas as pd
+import random
+
+winners_list=[]
+winner=""
+
+ player1 = ""
+ player2 = ""
 
 def import_word_file():
     df_words = pd.read_csv("C:/Users/gianm/Desktop/lista_parole.csv", sep =";")
-    #words =df_words("Gatto")
-    print(words)
+    words_list =df_words["PAROLE"]   
+    print(words_list)
+    return words_list
 
-import_word_file()
+def chosen_word(w_list):
+    #print(len(w_list))
+    i = random.randint(0, len(w_list)-1)
+    print(w_list.iloc[i])
+
+def main():
+    wl = import_word_file()
+    word = chosen_word(wl)
+   
+
+if __name__ == "__main__":
+    main()
+else:
+    print("Errore nell'avvio dell'applicazione")
