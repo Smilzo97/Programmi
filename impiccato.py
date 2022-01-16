aad0a3712f301098ff4912b201726693eaf5cab0
Smilzo97 <gianco.molteni@gmail.com>
2022-01-16 22:27:07 +0100

    inizio

--- impiccato.py
+++ impiccato.py
@@ -5,9 +5,11 @@ import string
 winners_list=[]
 winner = ""
 sequence ="_ "
+word = " "
 
 def import_word_file():
-    df_words = pd.read_csv("C:/Users/gianm/Desktop/lista_parole.csv", sep =";")
+    df_words = pd.read_csv("https://raw.githubusercontent.com/Smilzo97/Programmi/Inizio/lista_parole.csv")
+    #print(df_words)
     words_list =df_words["PAROLE"]   
     print(words_list)
     return words_list
@@ -18,14 +20,16 @@ def chosen_word(w_list):
     print(w_list.iloc[i])
     return w_list.iloc[i]
 
-def insert_players():
-    player1 = input('Inserisci nome giocatore1: ')
-    player2 = input('Inserisci nome giocatore2: ')
+def insert_player():
+    player = input('Inserisci nome giocatore: ')
+    return player
 
 def initialize():
     winner = ""
     wl = import_word_file()
+    global word
     word = str(chosen_word(wl))
+    global keyboard
     keyboard = []
     #print(word)
     for i in range(20 - (len(word))):
@@ -43,15 +47,33 @@ def turn (name):
     print(name + " tocca a te")
     print(sequence)
     print("Scegli una lettera")
+    inchar=input('Inserisci lettera: ')
+    state = check_char(inchar,word)
+    return state
+
+def reveal_char(c):
+    for i in range(len(word)):
+        if c == word[i]:
+            sequence[i] = c.upper()
+
+def check_char(c, word):
+    print(word)
+    if c in word:
+        print("letetra presente")
+        keyboard.remove(c.upper())
+        reveal_char(c)
+        print(sequence)
+        return True
 
 def main():
+    player1 = insert_player()
+    player2 = insert_player()
     while True:
-        insert_players()
         initialize()
-        
         #il gioco inizia
-        turn("player")
-        
+        #while ok:
+        turn(player1)
+        print(sequence)
         break
 
 if __name__ == "__main__":
