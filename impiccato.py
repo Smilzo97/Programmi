26ddc19ee3a2a5f09710352c560e186f37ad2d1e
Smilzo97 <Smilzo97@users.noreply.github.com>
2022-01-16 21:14:59 +0100

    inizio turno

--- impiccato.py
+++ impiccato.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import random
+import string
 
 winners_list=[]
-winner=""
-
- player1 = ""
- player2 = ""
+winner = ""
+sequence ="_ "
 
 def import_word_file():
     df_words = pd.read_csv("C:/Users/gianm/Desktop/lista_parole.csv", sep =";")
@@ -17,11 +16,43 @@ def chosen_word(w_list):
     #print(len(w_list))
     i = random.randint(0, len(w_list)-1)
     print(w_list.iloc[i])
+    return w_list.iloc[i]
+
+def insert_players():
+    player1 = input('Inserisci nome giocatore1: ')
+    player2 = input('Inserisci nome giocatore2: ')
+
+def initialize():
+    winner = ""
+    wl = import_word_file()
+    word = str(chosen_word(wl))
+    keyboard = []
+    #print(word)
+    for i in range(20 - (len(word))):
+        keyboard.append(random.choice(string.ascii_letters).upper())  
+    #print(keyboard)
+    for char in word:
+        global sequence
+        sequence = sequence + "_ "
+        keyboard.append(char.upper())  
+    #print(keyboard)
+    random.shuffle(keyboard)
+    print(keyboard)
+
+def turn (name):
+    print(name + " tocca a te")
+    print(sequence)
+    print("Scegli una lettera")
 
 def main():
-    wl = import_word_file()
-    word = chosen_word(wl)
-   
+    while True:
+        insert_players()
+        initialize()
+        
+        #il gioco inizia
+        turn("player")
+        
+        break
 
 if __name__ == "__main__":
     main()
