38191ae7fe7017e2a09efcf083c70d250acd3f40
Smilzo97 <Smilzo97@users.noreply.github.com>
2022-01-19 22:16:17 +0100

    Gioco finito!

--- impiccato.py
+++ impiccato.py
@@ -1,3 +1,4 @@
+#importo librerie
 import pandas as pd
 import random
 import string
@@ -5,94 +6,113 @@ import string
 sequence = []
 word = " "
 
+#dichiaro funzione per importare file csv con le parole
 def import_word_file():
+    #uso libreria Pandas per la gestione del file csv
     df_words = pd.read_csv("https://raw.githubusercontent.com/Smilzo97/Programmi/Inizio/lista_parole.csv")
-    #print(df_words)
+    #il risultato dell'import è sottoforma di dataset
+    #attraverso la seguente riga, estraggo le parole nella colonna "PAROLE"
     words_list =df_words["PAROLE"]   
-    #print(words_list)
     return words_list
 
 def chosen_word(w_list):
-    #print(len(w_list))
-    i = random.randint(0, len(w_list)-1)
-    #print(w_list.iloc[i])
-    return w_list.iloc[i].upper()
+    #Genero un numero i randomico ed estraggo la parola in posizone i
+    iw = random.randint(0, len(w_list)-1)
+    #restituisco la parola in maiuscolo
+    return w_list.iloc[iw].upper()
 
 def insert_player():
+    #inserisco il nome del giocatore
     player = input('Inserisci nome giocatore: ')
     return player
 
 def initialize():
+    #genrto variabili 
     winner = ""
     global keyboard
     global word
     keyboard = []
-    #print(word)
+
+    #importo e scelgo la parola
     wl = import_word_file()
     word = str(chosen_word(wl))
-    for i in range(15 - (len(word))):
+    #popolo la mia tastiera con le lettere della parola e casuali
+    #sono tutte in maisucolo
+    i = 0
+    for i in range(18 - (len(word))):
         keyboard.append(random.choice(string.ascii_letters).upper())  
-    #print(keyboard)
+
+    #per ogni lettera nella parola da indovinare, genero un _ e lo aggiungo ad una lista
     for char in word:
         global sequence 
         sequence.append("_ ")
+        #aggiungo la lettera alla tastiera
         keyboard.append(char.upper())  
-    #print(keyboard)
+    #eseguo uno shuffle per mischiare le lettere
     random.shuffle(keyboard)
-    #print(keyboard)
     return word, sequence
 
 def reveal_char(c):
-    global ok
+    global ok_reveal
+    #controllo che la lettera inserita corrisponda ad una o più lettere nella parola da indovinare
+    i = 0
     for i in range(len(word)):
         if c == word[i]:
            global sequence
+           #sostituisco
            sequence[i] = c
+        
+        #controllo se ha indovinato o no
         if count == 0:
-           ok = False
+           ok_reveal = False
         else:
-            ok = True
-    #print(ok)
-    return ok
+            ok_reveal = True
+    
+    return ok_reveal
 
 def delete_char(c):
+    #controllo ogni elemento della lista ed elimino l'elemento in caso di corrispondenza
+    index = 0
     for index in keyboard:
-        #print(index)
         if index == c:
             keyboard.remove(index)
            
 def check_char(c, word, errors):
-    print(c)   
+    #controllo ogni lettera
     if c in word:
-        print("Lettera presente")
-        ok2 = reveal_char(c)
+        #se è presente la rilevo e la elimino
+        print("--  Lettera presente  --")
+        ok = reveal_char(c)
         delete_char(c)
-        #print(sequence)
-        #print(ok2)
+
     else:
-        print("Lettera NON presente")
+        print(" --  Lettera NON presente  --")
+        #se non è presente aumento il numero degli errori 
         errors = errors+1
         delete_char(c)
-        ok2 = False
-    return ok2, errors
+        ok = False
+    return ok, errors
 
-def check_word_status(sequence, word, count):
+#controllo se il giocatore ha vinto o no
+def check_word_status(sequence, word, err):
     mystr =""
     end = True
+    i = 0
     for i in sequence:
         mystr=mystr+i
 
     if mystr == word:
-        print()
+        print("")
         print("******************************************")
         print("HAI VINTO! La parola era: "+ word)
         print("Gioco finito")
         print("******************************************")
         end = False
 
-    if count == len(word):
+    if err == 6:
+        print("")
         print("******************************************")
-        print("HAI ESAURITO LE MOSSE! La parola era: "+word)
+        print("HAI PERSO! MOSSE ESAURITE! La parola era: "+word)
         print("Gioco finito")
         print("******************************************")
         end = False
@@ -113,33 +133,40 @@ def main():
         count = 0
         errors = 0
         play = True 
+        end_char =True
 
         while play:
             #print(name + " tocca a te")à
             in_seq = True
+            #mostro gli spazi e la tastiera
             print()
-            print("Mosse a disposizione: "+ str(len(word)-count))
+            print("Mosse a disposizione: "+ str(6-errors))
             print("Errori: "+ str(errors))
-            print()
+            print("")
             print(sequence)
+            print("")
             print(keyboard)
-            print("Scegli una lettera")
+            #print("Scegli una lettera")
             global inchar
+            #chiedo inserimento lettera. Finchè non inserisce una lettera presente nella tastiera, non va avanti
             while in_seq:
                 inchar=input('Inserisci lettera: ')
                 if inchar.upper() in keyboard:
                     in_seq = False       
                 else:
                     print("Inserisci un'altra lettera")
-                    print()         
-            check_char_res, errors = check_char(inchar.upper(),word, errors)
-            if check_char_res == True:
-                print()
-            else:
-                count= count+1
-            play = check_word_status(sequence, word, count)
-    
-        another_game = input("Vuoi fare un'altra partita (S/n)?    ")
+                    print()      
+            #controlli   
+            check_char_res, errors = check_char(inchar.upper(),word, errors)            
+            play = check_word_status(sequence, word, errors)
+            print()
+            print("----------------------------------------------------")
+        #chiedo se vuole rigiocare e finchè non inserisce S (Si) o n (no) non va avanti
+        while end_char:
+            another_game = input("Vuoi fare un'altra partita (S/n)?    ")
+            if another_game =="S" or another_game == "n":
+                end_char = False
+        #pulisco la sequenza
         sequence.clear()
 
         if another_game == "n":
